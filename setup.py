8149ad94a45c28a05558edf76754b8aa4e8d4e46
Tahsin <62146852+TahsinTariq@users.noreply.github.com>
2023-07-29 23:52:40 +0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+from my_pip_package import __version__
+
+setup(
+    name='banna_im',
+    version="dev",
+
+    url='https://github.com/TahsinTariq/banna_IM',
+    author='Tahsin Tariq',
+    py_modules=['banna_IM'],
+)
